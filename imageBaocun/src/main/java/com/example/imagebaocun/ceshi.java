2d1b49e1709a9e1edab54afbfeea77b558dc5826
yiquwuyou <13613958265@163.com>
2023-09-05 23:48:31 +0800

    表白墙

---
+++ imageBaocun/src/main/java/com/example/imagebaocun/ceshi.java
@@ -0,0 +1,39 @@
+package com.example.imagebaocun;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+
+@RestController
+public class ceshi {
+    @PostMapping("/upload")
+    public String uploadImage(@RequestParam("image") MultipartFile image) {
+        try {
+            // 获取上传的图片文件名
+            String originalFilename = image.getOriginalFilename();
+
+            // 设置存储目录的路径
+            String uploadDirectory = "D:\\code_github\\";
+
+            // 创建存储目录（如果不存在）
+            File directory = new File(uploadDirectory);
+            if (!directory.exists()) {
+                directory.mkdirs();
+            }
+            // 构建目标文件对象
+            File destination = new File(uploadDirectory + originalFilename);
+
+            // 保存上传的图片文件
+            image.transferTo(destination);
+
+            return "Image uploaded successfully!";
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "Image upload failed.";
+        }
+    }
+}
