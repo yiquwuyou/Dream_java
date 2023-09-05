5b25b4bf6b96da0ace893d4e653c1420cf052dfd
yiquwuyou <13613958265@163.com>
2023-09-05 23:48:39 +0800

    表白墙

---
+++ imageBaocun/src/main/java/com/example/imagebaocun/ImageBaocunApplication.java
@@ -0,0 +1,13 @@
+package com.example.imagebaocun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageBaocunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImageBaocunApplication.class, args);
+    }
+
+}
