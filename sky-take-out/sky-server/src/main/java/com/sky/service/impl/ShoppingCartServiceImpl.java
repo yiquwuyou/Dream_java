82b44b0c29fc3d8f42bc031cf01acad2141d614d
yiquwuyou <13613958265@163.com>
2023-10-10 00:58:28 +0800

    查看购物车代码开发

--- sky-take-out/sky-server/src/main/java/com/sky/service/impl/ShoppingCartServiceImpl.java
+++ sky-take-out/sky-server/src/main/java/com/sky/service/impl/ShoppingCartServiceImpl.java
@@ -73,7 +73,19 @@ public class ShoppingCartServiceImpl implements ShoppingCartService {
             shoppingCart.setCreateTime(LocalDateTime.now());
             shoppingCartMapper.insert(shoppingCart);
         }
+    }
 
-
+    /**
+     * 查看购物车
+     * @return
+     */
+    public List<ShoppingCart> showShoppingCart() {
+        // 获取到当前微信用户的id
+        Long userId = BaseContext.getCurrentId();
+        ShoppingCart shoppingCart = ShoppingCart.builder()
+                .userId(userId)
+                .build();
+        List<ShoppingCart> list = shoppingCartMapper.list(shoppingCart);
+        return list;
     }
 }
