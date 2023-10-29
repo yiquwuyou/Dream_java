d45f9995250975a7a7a90ab696ef9f88250f28b7
yiquwuyou <13613958265@163.com>
2023-10-29 18:21:33 +0800

    课程学习过程代码

--- BiTeSpringBootDemo/SpringBootDemo1/src/main/java/com/yiquwuyou/springbootdemo1/Controller/ParamController.java
+++ BiTeSpringBootDemo/SpringBootDemo1/src/main/java/com/yiquwuyou/springbootdemo1/Controller/ParamController.java
@@ -13,7 +13,7 @@ import java.util.Arrays;
 import java.util.List;
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/user1")
 public class ParamController {
 
     @RequestMapping("/m1")
