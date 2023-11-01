3b0220905749274e1d3fcf27ec990e7ab7598c6a
yiquwuyou <13613958265@163.com>
2023-11-01 17:16:24 +0800

    洗牌算法 —— card类

---
+++ 数据结构实现/data-structure/src/shufflecard/Card.java
@@ -0,0 +1,35 @@
+package shufflecard;
+
+public class Card {
+    private String suit;   // 花色
+    private int rank;      // 牌面值
+
+    public Card(String suit, int rank){
+        this.suit = suit;
+        this.rank = rank;
+    }
+
+    public String getSuit() {
+        return suit;
+    }
+
+    public void setSuit(String suit) {
+        this.suit = suit;
+    }
+
+    public int getRank() {
+        return rank;
+    }
+
+    public void setRank(int rank) {
+        this.rank = rank;
+    }
+
+    @Override
+    public String toString() {
+        return "Card{" +
+                "suit='" + suit + '\'' +
+                ", rank=" + rank +
+                '}';
+    }
+}
