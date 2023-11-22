685411a39c10344350ecf12a11c32d4f6863cdf2
yiquwuyou <13613958265@163.com>
2023-11-22 20:33:23 +0800

    Spring日志学习

--- BiTeSpringBootDemo/SpringBootDemo2/src/main/java/com/example/springboot2/facade/Main.java
+++ BiTeSpringBootDemo/SpringBootDemo2/src/main/java/com/example/springboot2/facade/Main.java
@@ -2,12 +2,13 @@ package com.example.springboot2.facade;
 
 public class Main {
     public static void main(String[] args) {
-//        HallLight hallLight = new HallLight();
-//        hallLight.on();
-//        LivingLight livingLight = new LivingLight();
-//        livingLight.on();
-//        DiningLight diningLight = new DiningLight();
-//        diningLight.on();
+
+        HallLight hallLight = new HallLight();
+        hallLight.on();
+        LivingLight livingLight = new LivingLight();
+        livingLight.on();
+        DiningLight diningLight = new DiningLight();
+        diningLight.on();
 
         FacadePattern facadePattern = new FacadePattern();
         facadePattern.lightOn();
