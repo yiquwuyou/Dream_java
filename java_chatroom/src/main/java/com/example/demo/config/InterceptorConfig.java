792910a4806032165397aa3f4cd05fab97a466d8
yiquwuyou <13613958265@163.com>
2023-12-13 19:37:56 +0800

    网页聊天室session改为jwt

--- java_chatroom/src/main/java/com/example/demo/config/InterceptorConfig.java
+++ java_chatroom/src/main/java/com/example/demo/config/InterceptorConfig.java
@@ -37,4 +37,5 @@ public class InterceptorConfig  extends WebMvcConfigurationSupport {
                 .excludePathPatterns("/login")
                 .excludePathPatterns("/WebSocketMessage");
     }
+
 }
